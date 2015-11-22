8d0db721f7fca3bcd26d416f5991684b55f6bcb2
LiciaMila <jenmarg@libero.it>
2015-11-22 13:55:16 +0100

    new files

--- main.R
+++
@@ -1,9 +0,0 @@
-
-library(RCurl)
-#options(RCurlOptions = list(verbose = FALSE, capath = system.file("CurlSSL", "cacert.pem", package = "RCurl"), ssl.verifypeer = FALSE))
-
-#x <- getURL("https://raw.githubusercontent.com/LiciaMila/Practical-Machine-Learning/master/pml-testing.csv", ssl.verifypeer = FALSE)
-y <- read.csv("https://raw.githubusercontent.com/LiciaMila/Practical-Machine-Learning/master/pml-training.csv")
-
-
-
