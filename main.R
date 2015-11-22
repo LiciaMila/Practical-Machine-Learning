b02a10f3b2dac6dddef8b3ea90c4b5d6d02004b3
LiciaMila <jenmarg@libero.it>
2015-11-22 11:07:38 +0100

    add other file

--- main.R
+++ main.R
@@ -1,7 +1,9 @@
 
 library(RCurl)
-x <- getURL("https://github.com/LiciaMila/Practical-Machine-Learning/pml-testing.csv")
-y <- read.csv(text = x)
+#options(RCurlOptions = list(verbose = FALSE, capath = system.file("CurlSSL", "cacert.pem", package = "RCurl"), ssl.verifypeer = FALSE))
+
+#x <- getURL("https://raw.githubusercontent.com/LiciaMila/Practical-Machine-Learning/master/pml-testing.csv", ssl.verifypeer = FALSE)
+y <- read.csv("https://raw.githubusercontent.com/LiciaMila/Practical-Machine-Learning/master/pml-training.csv")
 
 
 
